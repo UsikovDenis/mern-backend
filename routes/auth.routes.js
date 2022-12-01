a061685b1e083012da7e192cda804a2dfbc81e7f
denis <denis.usikov95@mail.ru>
2022-12-01 22:24:09 +0300

    first commit

---
+++ routes/auth.routes.js
@@ -0,0 +1,92 @@
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/default.json");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const router = Router();
+
+router.post(
+  "/register",
+  [
+    check("email", "Некорректный email").isEmail(),
+    check("password", "Минимальная длина пароля 6 символов").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Некорректные данные при регистрации",
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "Такой пользователь уже существует" });
+      }
+      const heshedPassword = await bcrypt.hash(password, 15);
+      const user = new User({ email, password: heshedPassword });
+
+      await user.save();
+
+      res.status(201).json({ message: "Пользователь создан" });
+    } catch (e) {
+      res.status(500).json({ message: "Что-то не так, но вы держитесь" });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    check("email", "Некорректный email").normalizeEmail().isEmail(),
+    check("password", "Минимальная длина пароля 6 символов")
+      .exists()
+      .isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Некорректные данные при входе",
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: "Неверные данные при авторизаниии" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ message: "Неверные данные при авторизаниии" });
+      }
+
+      const token = jwt.sign({ userId: user.id }, config.get("jwtSecret"), {
+        expiresIn: "1h",
+      });
+
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res.status(500).json({ message: "Что-то не так, но вы держитесь" });
+    }
+  }
+);
+
+module.exports = router;
