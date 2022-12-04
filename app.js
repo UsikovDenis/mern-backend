ff0f1424b57bdbf3fbe7ebe1a6b758533db9f6c1
denis <denis.usikov95@mail.ru>
2022-12-04 10:23:49 +0300

    frontend+backend proxy

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
 
+
 const app = express();
+
+app.use(express.json({extended: true}))
+
 
 app.use("/api/auth", require("./routes/auth.routes"));
 
