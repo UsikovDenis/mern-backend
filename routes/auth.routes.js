e8493c04cebe33f386c3382f60ca480e258f0ac5
denis <denis.usikov95@mail.ru>
2022-12-05 02:27:49 +0300

    auth.router no error

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const config = require("../config/default.json");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
+const { Router } = require("express");
 const router = Router();
 
 
@@ -47,14 +47,14 @@ router.post(
 );
 // /api/auth/login
 router.post(
-  "/login",
-  [
-    check("email", "Некорректный email").normalizeEmail().isEmail(),
-    check("password", "Минимальная длина пароля 6 символов")
-      .exists()
-      .isLength({ min: 6 }),
-  ],
-  async (req, res) => {
+	"/login",
+	[
+		check("email", "Некорректный email").isEmail(),
+		check("password", "Минимальная длина пароля 6 символов").isLength({
+			min: 6,
+		}),
+	],
+	async (req, res) => {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -70,23 +70,26 @@ router.post(
       if (!user) {
         return res
           .status(400)
-          .json({ message: "Неверные данные при авторизаниии" });
+          .json({ message: "Неверные данные при авторизации" });
       }
 
-      const isMatch = await bcrypt.compare(password, user.password);
+	    const isMatch = await bcrypt.compare(password, user.password)
+
       if (!isMatch) {
         return res
           .status(400)
-          .json({ message: "Неверные данные при авторизаниии" });
+          .json({ message: "Неверные данные при авторизации" });
       }
 
-      const token = jwt.sign({ userId: user.id }, config.get("jwtSecret"), {
-        expiresIn: "1h",
-      });
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+      )
 
-      res.json({ token, userId: user.id });
+      res.status(200).json({ token, userId: user.id } )
     } catch (e) {
-      res.status(500).json({ message: "Что-то не так, но вы держитесь" });
+      res.status(500).json({ message: "Что-то не так, но вы держитесь" })
     }
   }
 );
